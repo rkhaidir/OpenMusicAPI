746680d0a6a910d1eba23295894990ce1f6afd8b
rkhaidir <rkhaidir085@gmail.com>
2023-06-08 00:34:43 +0800

    albums fitur

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+// album
+const album = require('./api/albums');
+const AlbumService = require('./services/postgres/AlbumsService');
+const AlbumValidator = require('./validator/albums');
+
+const init = async () => {
+  const albumService = new AlbumService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: album,
+    options: {
+      service: albumService,
+      validator: AlbumValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
